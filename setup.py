1cbf4120af12d9d00f20418cd0ae1698134d6da0
jdnascim <jose.dori.nascimento@gmail.com>
2019-09-20 15:32:43 -0300

    new version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 setup(
     name='social-tracker-library',  # Required
 
-    version='0.2.6',  # Required
+    version='0.2.7',  # Required
 
     description='Python library to communicate with the Social Tracker \
     application',  # Optional
