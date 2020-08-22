0a073cb9eb9b27215fc75b373abbb4dd200cc2eb
ra170862 <ra170862@ic.unicamp.br>
2020-08-22 11:27:08 -0300

    new version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 setup(
     name='social-tracker-library',  # Required
 
-    version='0.2.28',  # Required
+    version='0.2.29',  # Required
 
     description='Python library to communicate with the Social Tracker \
     application',  # Optional
